529fed00a5376486509000fd15e6a475ce7298c5
intenseG <intenseg1515@gmail.com>
2021-07-09 09:11:12 +0900

    fetchとasync/awaitを使用するよう修正

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentCategory = 'vegetable';
+const categoryElm = document.getElementById('category');
+const itemElm = document.getElementById('item');
+
+async function changeCategory(event) {
+    currentCategory = event.currentTarget.value;
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    console.log(data);
+    const target = data[currentCategory];
+
+    for (let i = itemElm.options.length - 1; 0 <= i; --i) {
+        itemElm.remove(i);
+    }
+
+    for (let j = 0; j < target.length; j++) {
+        const opElm = document.createElement('option');
+        opElm.setAttribute('value', target[j].value);
+        opElm.textContent = target[j].name;
+        itemElm.appendChild(opElm);
+    }
+}
+
+categoryElm.addEventListener('change', changeCategory);
